b832a7ad88fea692d9b75cc56833bac6bbbc1f83
Richard Paul <richardpaulraj2@gmail.com>
2024-02-21 13:50:09 +0530

    Removed Background script and modifies Content Script code

--- contentScript.js
+++ contentScript.js
@@ -18,61 +18,53 @@ const observe = () => {
     '.ytp-advanced-playback-button'
   )
   if (settings && !boost) {
-    observe.busy = true //observe.busy
-    chrome.storage.local.get(
-      {
-        boost: 2, //If boost is not present it will use the default value 2 and It will look for this arg ( args: [res.boost] ) which sends the boost value from the background
-      },
-      (res) => {
-        const boost = Object.assign(settings.cloneNode(true), {
-          textContent: '',
-          style: '',
-          title: `Volume Booster`,
-        })
-        boost.classList.replace('ytp-settings-button', 'ytp-boost-button')
+    const boost = Object.assign(settings.cloneNode(true), {
+      textContent: '',
+      style: '',
+      title: `Volume Booster`,
+    })
+    boost.classList.replace('ytp-settings-button', 'ytp-boost-button')
 
-        const svg = document.createElementNS(svgns, 'svg')
-        svg.setAttribute('height', '100%')
-        svg.setAttribute('width', '100%')
-        svg.setAttribute('viewBox', '0 0 42 42')
+    const svg = document.createElementNS(svgns, 'svg')
+    svg.setAttribute('height', '100%')
+    svg.setAttribute('width', '100%')
+    svg.setAttribute('viewBox', '0 0 42 42')
 
-        // Create image element
-        const image = document.createElementNS(
-          'http://www.w3.org/2000/svg',
-          'image'
-        )
-        image.setAttribute(
-          'href',
-          'https://img.icons8.com/pastel-glyph/64/loudspeaker--v2.png'
-        )
-        image.classList.add('volumeBooster')
-        image.setAttribute('width', '24')
-        image.setAttribute('height', '24')
-        image.setAttribute('x', '9')
-        image.setAttribute('y', '9')
-        image.classList.add('colorizedImg')
+    // Create image element
+    const image = document.createElementNS(
+      'http://www.w3.org/2000/svg',
+      'image'
+    )
+    image.setAttribute(
+      'href',
+      'https://img.icons8.com/pastel-glyph/64/loudspeaker--v2.png'
+    )
+    image.classList.add('volumeBooster')
+    image.setAttribute('width', '24')
+    image.setAttribute('height', '24')
+    image.setAttribute('x', '9')
+    image.setAttribute('y', '9')
+    image.classList.add('colorizedImg')
 
-        // Append image to SVG
-        svg.appendChild(image)
+    // Append image to SVG
+    svg.appendChild(image)
 
-        boost.appendChild(svg)
+    boost.appendChild(svg)
 
-        settings.parentNode.insertBefore(boost, settings)
+    settings.parentNode.insertBefore(boost, settings)
 
-        observe.busy = false
+    // Only if the wants to change the value
+    let boostButtonClicked = false
 
-        // Only if the wants to change the value
-        boost.addEventListener('click', (e) => {
-          //Test
-
-          // Modal code
-          const modalHTML = `<div id="modalVolumeBooster">
+    boost.addEventListener('click', (e) => {
+      // Modal code
+      const modalHTML = `<div id="modalVolumeBooster">
   <div id="modal-header">
     <p>Volume Booster</p>
     <div id="closeNEW"><span>X</span></div>
   </div>
   <div id="singleLine"></div>
-  <div class="rangeContainer">
+  <div class="rangeContainerNEW">
     Boost :
     <input
       type="range"
@@ -89,111 +81,95 @@ const observe = () => {
   </div>
 </div>`
 
-          player.appendChild(
-            document.createRange().createContextualFragment(modalHTML)
-          )
-
-          const modal = document.getElementById('modalVolumeBooster')
-          const slider = document.getElementById('advancedPlaybackSliderNEW')
-          const modalText = document.getElementById('sliderValueNEW')
-          const closeBtn = document.getElementById('closeNEW')
-          const defaultButton = document.getElementById('defaultButtonNEW')
-
-          if (modal.style.display === 'block') {
-            modal.style.display = 'none'
-          } else {
-            modal.style.display = 'block'
-          }
-
-          closeBtn.addEventListener('click', () => {
-            console.log('close btn clicked')
-            modal.style.display = 'none'
-          })
-
-          slider.addEventListener('mousemove', (e) => {
-            modalText.textContent = slider.value + 'x'
-          })
-
-          defaultButton.addEventListener('click', () => {
-            console.log('deafult btn clicked') //Not Working
-            slider.value = 1
-            modalText.textContent = slider.value + 'x'
-
-            const video = document.querySelector('video') //I directly used the video element you can also do with 'const player'
-            if (video) {
-              video.playbackRate = slider.value
-            }
-          })
-
-          slider.addEventListener('input', () => {
-            // const video = document.querySelector('video') //I directly used the video element you can also do with 'const player'
-            // if (video) {
-            //   video.playbackRate = slider.value
-            // }
-          })
-          //TEst
-
-          if (e.shiftKey) {
-            chrome.storage.local.get(
-              {
-                boost: 2,
-              },
-              (res) => {
-                const val = prompt(
-                  'Insert the new boosting level (2, 3, 4, 5 or 6)',
-                  res.boost
-                )?.trim()
-
-                if (
-                  val === '2' ||
-                  val === '3' ||
-                  val === '4' ||
-                  val === '5' ||
-                  val === '6'
-                ) {
-                  text.textContent = val + 'x'
-                  chrome.storage.local.set({
-                    boost: parseInt(val),
-                  })
-                  chrome.runtime.sendMessage({
-                    method: 'adjust_boost',
-                    boost: parseInt(val),
-                  })
-                }
-              }
-            )
-            return
-          }
-
-          if (boost.classList.contains('boosting')) {
-            chrome.runtime.sendMessage(
-              {
-                method: 'revoke_boost',
-              },
-              () => {
-                boost.classList.remove('boosting')
-                // boost.title = msg.replace('currentStatusTemp', 'disabled')
-              }
-            )
-          } else {
-            chrome.runtime.sendMessage(
-              {
-                method: 'apply_boost',
-              },
-              (r) => {
-                if (r === true || r === 'true') {
-                  boost.classList.add('boosting')
-                  // boost.title = msg.replace('currentStatusTemp', 'enabled')
-                } else {
-                  alert('Cannot boost this video: ' + r)
-                }
-              }
-            )
-          }
+      const player = [...document.querySelectorAll('.html5-video-player')]
+        .sort((a, b) => {
+          return b.offsetHeight - a.offsetHeight
         })
+        .shift() //Actually there are multiple html5 video player but we a picking the largest one using shift()
+
+      player.appendChild(
+        document.createRange().createContextualFragment(modalHTML)
+      )
+
+      const modal = document.getElementById('modalVolumeBooster')
+
+      if (modal.style.display === 'block') {
+        modal.style.display = 'none'
+      } else {
+        modal.style.display = 'block'
       }
-    )
+
+      if (boostButtonClicked) {
+        return
+      }
+
+      boostButtonClicked = true
+
+      const video = player.querySelector('video')
+
+      if (!video) {
+        console.error('No video element found.')
+        return
+      }
+
+      if (!video.booster) {
+        const context = new AudioContext()
+        const source = context.createMediaElementSource(video)
+        const preamp = context.createGain()
+
+        preamp.gain.value = 1 // Set initial volume boost
+
+        source.connect(preamp)
+        preamp.connect(context.destination)
+
+        video.booster = {
+          context: context,
+          source: source,
+          preamp: preamp,
+        }
+      }
+
+      const slider = document.getElementById('advancedPlaybackSliderNEW')
+      const modalText = document.getElementById('sliderValueNEW')
+      const closeBtn = document.getElementById('closeNEW')
+      const defaultButton = document.getElementById('defaultButtonNEW')
+
+      if (modal.style.display === 'block') {
+        modal.style.display = 'none'
+      } else {
+        modal.style.display = 'block'
+      }
+
+      closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none'
+      })
+
+      slider.addEventListener('mousemove', (e) => {
+        modalText.textContent = slider.value + 'x'
+      })
+
+      defaultButton.addEventListener('click', () => {
+        slider.value = 1
+        modalText.textContent = slider.value + 'x'
+
+        const video = document.querySelector('video')
+
+        if (video && video.booster) {
+          video.booster.preamp.gain.value = 1 // Reset volume booster to 1
+        }
+      })
+
+      slider.addEventListener('input', () => {
+        const video = player.querySelector('video')
+
+        if (video && video.booster) {
+          video.booster.preamp.gain.value = slider.value // Set volume booster gain to slider value
+        }
+      })
+      //TEst
+    })
   }
+
   if (settings && !advancedPlayback) {
     const advancedPlayback = Object.assign(settings.cloneNode(true), {
       textContent: '',
@@ -237,7 +213,7 @@ const observe = () => {
 <div id="modal">
   <div id="modal-header">
     <p>Playback Speed</p>
-    <div id="close"><span>X</span></div>
+    <div id="closePlaybackSpeed"><span>X</span></div>
   </div>
   <div id="singleLine"></div>
   <div class="rangeContainer">
@@ -266,7 +242,7 @@ const observe = () => {
     const modal = document.getElementById('modal')
     const slider = document.getElementById('advancedPlaybackSlider')
     const modalText = document.getElementById('sliderValue')
-    const closeBtn = document.getElementById('close')
+    const closeBtn = document.getElementById('closePlaybackSpeed')
     const defaultButton = document.getElementById('defaultButton')
 
     advancedPlayback.addEventListener('click', (e) => {
